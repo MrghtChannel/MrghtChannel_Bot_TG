161fbe22547b79163bc6af8990fa687445ca9154
MrghtChannel <mrghtchannel@gmail.com>
2025-01-26 13:48:01 +0100

    0.4

--- api/webhook.js
+++ api/webhook.js
@@ -7,9 +7,14 @@ const bot = new TelegramBot(token, { polling: false });
 module.exports = async (req, res) => {
   if (req.method === 'POST') {
     const json = req.body;
-    await bot.handleUpdate(json);
 
-    res.status(200).send('OK');
+    try {
+      await bot.handleUpdate(json);
+      res.status(200).send('OK');
+    } catch (error) {
+      console.error('Error handling webhook:', error);
+      res.status(500).send('Internal Server Error');
+    }
   } else {
     res.status(405).send('Method Not Allowed');
   }
